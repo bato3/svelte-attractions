016b8f7553fc2c012655730bb7049f1898c59e93
bato3 (work) <bato3@bandyci.org>
2021-12-29 12:32:29 +0100

    add scss support `npm i -D node-sass svelte-preprocess`

--- src/App.svelte
+++ src/App.svelte
@@ -6,20 +6,24 @@
 	<h1>Hello {name}!</h1>
 </main>
 
-<style>
+<style lang="scss">
+    $red: #ff3e00;
+    $red: blue;
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
+
 
 	h1 {
-		color: #ff3e00;
+		color: $red;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
+}
+
 
 	@media (min-width: 640px) {
 		main {
