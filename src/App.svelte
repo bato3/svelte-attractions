9af53435acaa8ad4da69901b87bcad5e4d559263
bato3 (work) <bato3@bandyci.org>
2021-12-30 14:30:16 +0100

    Add `bootstrap` to project

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,38 @@
 <script lang="ts">
-    import { Button, TextField, H1 } from 'attractions';
+    import { TextField, H1, Button } from "attractions";
+
     //import {}  from 'svelte-feather-icons/src/icons/AtSignIcon.svelte'
-    import {AtSignIcon} from 'svelte-feather-icons';
-    import {CloudAlerting32, Awake32, CircleFilled32} from '@carbon/icons';
+    import { AtSignIcon } from "svelte-feather-icons";
+    import { CloudAlerting32, Awake32, CircleFilled32 } from "@carbon/icons";
 
-    import { TouchId } from '@carbon/pictograms';
-    import  CrIcon from './components/CrIcon.svelte';
+    import { TouchId } from "@carbon/pictograms";
+    import CrIcon from "./components/CrIcon.svelte";
 
-
-    let name:string =  'World';
+    let name: string = "World";
 </script>
 
-  <Button>click me</Button>
+<Button>click me</Button>
 <H1>
-  <CrIcon icon={CloudAlerting32}/>
-  <CrIcon icon={Awake32}/>
-  <CrIcon icon={CircleFilled32}/>
-  <CrIcon icon={TouchId} width=16 height=16/>
+    <CrIcon icon={CloudAlerting32} />
+    <CrIcon icon={Awake32} />
+    <CrIcon icon={CircleFilled32} />
+    <CrIcon icon={TouchId} width="16" height="16" />
 </H1>
 <TextField outline label="Username" withItem type="email">
-  <AtSignIcon size="24" class="item" />
+    <AtSignIcon size="24" class="item" />
 </TextField>
-  <main>
-	<h1 class="something">Hello {name}!</h1>
-</main>  <style lang="scss">
+<main>
+    <h1 class="something">Hello {name}!</h1>
+</main>
 
+<style lang="scss">
     /*
       // when you don use theme file
       @use '~attractions/_variables' as vars;
     //*/
-    @use 'theme' as vars;
-    @use '~attractions/_mixins';
-    @use '~attractions/_appearances';
+    @use "theme" as vars;
+    @use "~attractions/_mixins";
+    @use "~attractions/_appearances";
 
     .something {
         color: vars.$main;
@@ -40,27 +41,24 @@
         @include appearances.button;
     }
 
-
     $red: vars.$something-else;
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
 
+        h1 {
+            color: $red;
+            text-transform: uppercase;
+            font-size: 4em;
+            font-weight: 100;
+        }
+    }
 
-	h1 {
-		color: $red;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-}
-
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
